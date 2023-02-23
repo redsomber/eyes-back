91a5ab30d424fe2f5fd055254c1b1c19c730f6a3
Red Somber <veyder0999@gmail.com>
2023-02-23 20:43:30 +0300

    cors headers

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ app.use(express.json());
 app.use(cors({
   origin: '*'
 }))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: { origin: "*" } });
 
